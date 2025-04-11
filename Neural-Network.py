2ee1d899f771ff4902f31d7a2d05d028bca7fde5
SanjayKumar <0508sanjaykumar@gmail.com>
2025-04-11 09:54:52 +0530

    Added Neural Network

---
+++ Neural-Network.py
@@ -0,0 +1,16 @@
+import numpy as np
+# Inputs (XOR truth table)
+X = np.array([
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1]
+])
+
+# Outputs (XOR results)
+y = np.array([
+    [0],
+    [1],
+    [1],
+    [0]
+])
