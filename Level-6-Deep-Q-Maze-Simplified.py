1c8c592b2a211028791fc224d03076ddb8ad1982
SanjayKumar <0508sanjaykumar@gmail.com>
2025-05-04 17:54:49 +0530

    feat: Added foward and module actually

---
+++ Level-6-Deep-Q-Maze-Simplified.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+
+class QNetwork(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(QNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size,128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, output_size) 
+
+    def forward(self,x):
+        x = torch.relu(self.fc1(x))
+        x = torch.rele(self.fc2(x))
+        return self.fc3(x)
+    
+#initialise Q-network
+
